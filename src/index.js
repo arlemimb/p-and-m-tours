1e9af6d85daeacb3e8c71e0aee79c8e2aaacff0e
MB <mel@arlemi.com>
2022-07-29 13:01:52 +0100

    Merging everything into the more Advanced webpack/babel layout, there are breaking changes in terms of the layout

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import 'dotenv/config';
-
 import App from './App';
 
 const title = 'P & M Tours';
 
-console.log('Helllo Node.js project');
-
-console.log(process.env.TEST_VAR);
-
 ReactDOM.render(
-  //<App title={title} />,
+  <App title={title} />,
   document.getElementById('app')
 );
 
