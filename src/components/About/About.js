1d1583b7455b529f67984219060f58b13406df57
MB <mel@arlemi.com>
2022-08-01 20:06:36 +0100

    Forgot to do teh arrow functions, updated now

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function About() {
+const About = () => {
   return (
     <div>
       <h1>About</h1>
     </div>
   );
-}
+};
+
+export default About;
--- src/components/Cuisine/Cuisine.js
+++ src/components/Cuisine/Cuisine.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default function Cuisine() {
+const Cuisine = () => {
   return (
     <div>
       <h1>Cuisine</h1>
     </div>
   );
-}
+};
+
+export default Cuisine;
